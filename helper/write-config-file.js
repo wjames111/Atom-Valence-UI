90c526b023dbdf890f24f54981f4380e216461c7
William James <me@williamhjames.com>
2020-06-03 14:30:32 -0400

    adds color setting for workspace border

--- helper/write-config-file.js
+++ helper/write-config-file.js
@@ -1,29 +1,29 @@
-'use babel';
-
-import fs from 'fs';
-
-function writeConfigFile(content, reload = false) {
-  alert('hello')
-    return new Promise((resolve, reject) => {
-        if (!content) return reject({ success: false, error: 'No content given' });
-
-        fs.writeFile(`${__dirname}/../styles/user-settings.less`, content, 'utf8', (error) => {
-            if (error) return reject({ success: false, error: 'Failed to write settings file' });
-
-            if (reload) {
-                const amuPackage = atom.packages.getLoadedPackage('my-package');
-
-                if (amuPackage) {
-                    amuPackage.deactivate();
-                    setImmediate(() => amuPackage.activate());
-                }
-            }
-
-            return resolve({ success: true, error: null });
-        });
-
-        return resolve({ success: true, error: null });
-    });
-}
-
-export default writeConfigFile;
+// 'use babel';
+//
+// import fs from 'fs';
+//
+// function writeConfigFile(content, reload = false) {
+//   alert('hello')
+//     return new Promise((resolve, reject) => {
+//         if (!content) return reject({ success: false, error: 'No content given' });
+//
+//         fs.writeFile(`${__dirname}/../styles/user-settings.less`, content, 'utf8', (error) => {
+//             if (error) return reject({ success: false, error: 'Failed to write settings file' });
+//
+//             if (reload) {
+//                 const amuPackage = atom.packages.getLoadedPackage('my-package');
+//
+//                 if (amuPackage) {
+//                     amuPackage.deactivate();
+//                     setImmediate(() => amuPackage.activate());
+//                 }
+//             }
+//
+//             return resolve({ success: true, error: null });
+//         });
+//
+//         return resolve({ success: true, error: null });
+//     });
+// }
+//
+// export default writeConfigFile;
